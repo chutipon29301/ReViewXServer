052ab9336f2bb93e306c51c5807af59811279005
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-08 15:38:44 +0700

    Add genre post request

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ var app = express();
 
 app.listen(8000);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function(req,res,next){
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(function (req, res, next) {
     // Allow access from other domain
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+app.use(express.static('public'))
 
 MongoClient.connect('mongodb://127.0.0.1:27017/ReviewXServer', function (err, db) {
     if (err) {
@@ -22,5 +25,75 @@ MongoClient.connect('mongodb://127.0.0.1:27017/ReviewXServer', function (err, db
         return;
     }
     console.log('Successful connect to database');
-    
+
+    app.post('post/v1/addGenre', (req, res) => {
+        if (!(req.body.name, req.body.link)) {
+            return res.status(404).send({
+                err: -1,
+                msg: 'Bad Request'
+            });
+        }
+        db.collection('genre').insertOne({
+            name: req.body.name,
+            link: req.body.link
+        }, (err, result) => {
+            if (err) {
+                return res.status(500).send(err)
+            }
+            res.status(200).send('OK');
+        });
+    });
+
+    app.post('post/v1/deleteGenre', (req, res) => {
+        if (!req.body.genreID) {
+            return res.status(404).send({
+                err: -1,
+                msg: 'Bad Request'
+            });
+        }
+        db.collection('genre').deleteOne({
+            _id: ObjectID(req.body.genreID)
+        }, (err, result) => {
+            if (err) {
+                return res.status(500).send(err);
+            }
+            res.status(200).send('OK');
+        });
+    });
+
+    app.post('post/v1/listGenre', (req, res) => {
+        db.collection('genre').find({}).toArray().then(result => {
+            for (let i = 0; i < result.length; i++) {
+                result[i].genreID = result[i]._id;
+                delete result[i]._id;
+            }
+            res.status(200).send(result);
+        });
+    });
+
+    app.post('post/v1/addUser', (req, res) => {
+        if (!(req.body.facebookID && req.body.preference)) {
+            return res.status(404).send({
+                err: -1,
+                msg: 'Bad Request'
+            });
+        }
+        db.collection('user').insertOne({
+            _id: req.body.facebookID,
+            preference: req.body.preference
+        }, (err, result) => {
+            if (err) {
+                switch (err.code) {
+                    case 11000:
+                        return res.status(200).send({
+                            err: 0,
+                            msg: 'User Exist'
+                        });
+                    default:
+                        return res.status(500).send(err);
+                }
+            }
+            res.status(200).send('OK');
+        });
+    });
 });
