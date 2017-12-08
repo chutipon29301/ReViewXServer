fdaf445b16c8bc7268e9110a00c378793f2deb68
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2017-12-08 14:31:36 +0700

    Create python enviroment and install Django

---
+++ reviewxenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/admin/Documents/Semester3/Java/Project/ReViewXServer/reviewxenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
